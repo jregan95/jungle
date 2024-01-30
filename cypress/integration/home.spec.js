99b2780e910be313ac6adff9fb6a275c5d105ac5
Jill Regan <jillian.jnr@hotmail.com>
2024-01-30 14:09:23 -0500

    Create: cypress tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+//<reference types="cypress" />
+
+describe('example to-do app', () => {
+  it('successfully loads', () => {
+    cy.visit('http://localhost:3000') // change URL to match your dev URL
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
